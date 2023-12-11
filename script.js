25bb275d3a417974369cd98ac05a28fef993d805
dhanush3727 <dhanushs4827@gmail.com>
2023-12-11 12:30:11 +0530

    TP V3

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector(".menu-bar");
-const navList = document.querySelector(".nav-list");
+const navList = document.querySelector(".navList");
+const navLists = document.querySelectorAll(".nav-list");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   navList.classList.toggle("show");
+});
+navLists.forEach((list) => {
+  list.addEventListener("click", () => {
+    menu.classList.remove("active");
+    navList.classList.remove("show");
+  });
 });
